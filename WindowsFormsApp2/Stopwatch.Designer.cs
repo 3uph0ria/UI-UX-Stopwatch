5259f46b7423c80f70a37edad762ad12fd45423e
3uph0ria <s33rgey@bk.ru>
2021-01-08 16:41:04 +0300

    Stopwatch.

---
+++ WindowsFormsApp2/Stopwatch.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Stopwatch
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Stopwatch));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(73, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Image = global::WindowsFormsApp2.Properties.Resources.timer;
+            this.label2.Location = new System.Drawing.Point(95, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Image = global::WindowsFormsApp2.Properties.Resources.timer;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 190);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Stopwatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(213, 209);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Stopwatch";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Stopwatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp2/Stopwatch.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Stopwatch : Form
+    {
+        DateTime date;
+        public Stopwatch()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (button2.Text == "Start")
+            {
+                button2.Text = "Stop";
+                label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(50)))), ((int)(((byte)(84)))));
+                label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(50)))), ((int)(((byte)(84)))));
+                label1.Image = global::WindowsFormsApp2.Properties.Resources.timeract;
+                date = DateTime.Now;
+                Timer timer = new Timer();
+                timer.Interval = 10;
+                timer.Tick += new EventHandler(tickTimer);
+                timer.Start();
+            } else
+            {
+                button2.Text = "Start";
+                label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+                label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+                label1.Image = global::WindowsFormsApp2.Properties.Resources.timer;
+                label1.Text = "00:00";
+                label2.Text = "00";
+
+            }
+        }
+        private void tickTimer(object sender, EventArgs e)
+        {
+            if (button2.Text == "Stop")
+            {
+                long tick = DateTime.Now.Ticks - date.Ticks;
+                DateTime stopWatch = new DateTime();
+                stopWatch = stopWatch.AddTicks(tick);
+                label1.Text = String.Format("{0:mm:ss}", stopWatch);
+                label2.Text = String.Format("{0:ff}", stopWatch);
+            } 
+            else
+            {
+                label1.Text = "00:00";
+                label2.Text = "00";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Start")
+            {
+                button2.Text = "Stop";
+                label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(50)))), ((int)(((byte)(84)))));
+                label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(50)))), ((int)(((byte)(84)))));
+                label1.Image = global::WindowsFormsApp2.Properties.Resources.timeract;
+                date = DateTime.Now;
+                Timer timer = new Timer();
+                timer.Interval = 10;
+                timer.Tick += new EventHandler(tickTimer);
+                timer.Start();
+            }
+            else
+            {
+                button2.Text = "Старт";
+                label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+                label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+                label1.Image = global::WindowsFormsApp2.Properties.Resources.timer;
+                label1.Text = "00:00";
+                label2.Text = "00";
+
+            }
+        }
+    }
+}
